77402b5bdc0ad306f0aceab6f201d9fe8d0368f9
Deepak <54376136+Deepak1609@users.noreply.github.com>
2021-08-09 16:23:07 +0530

    Move Zeroes in c++

---
+++ Move Zeroes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+//         for( int i =0; i<nums.size()-1; i++)
+//         {
+//             if(nums[i]==0)
+//             {
+//                 swap(nums[i],nums[i+1]);
+//             }
+//         }
+        int n = nums.size();
+        if(n<=1)return;
+        int z = 0;
+        int j = 1;
+        while(j < n){
+            if(nums[z] == 0 && nums[j]!=0){
+                swap(nums[z],nums[j]);
+                z++;               
+            }
+            if(nums[z]!=0)z++;
+             j++;
+    }}
+};
