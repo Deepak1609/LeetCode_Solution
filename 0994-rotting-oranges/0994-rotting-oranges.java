b2d1443a98abfa2120aec5b052f88875cc1fd2e2
Deepak <54376136+Deepak1609@users.noreply.github.com>
2026-03-27 13:08:52 +0530

    Time: 2 ms (90.5%), Space: 44.3 MB (49.09%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        int fresh = 0;
+
+        // Step 1: Initialize
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
+                }
+                if (grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        // edge case
+        if (fresh == 0) return 0;
+
+        int minutes = 0;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        // Step 2: BFS
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            boolean infected = false;
+
+            for (int i = 0; i < size; i++) {
+                int[] curr = queue.poll();
+
+                for (int[] d : dirs) {
+                    int ni = curr[0] + d[0];
+                    int nj = curr[1] + d[1];
+
+                    if (ni >= 0 && nj >= 0 && ni < m && nj < n && grid[ni][nj] == 1) {
+                        grid[ni][nj] = 2;
+                        queue.offer(new int[]{ni, nj});
+                        fresh--;
+                        infected = true;
+                    }
+                }
+            }
+
+            if (infected) minutes++;
+        }
+
+        return fresh == 0 ? minutes : -1;
+        
+    }
+}
